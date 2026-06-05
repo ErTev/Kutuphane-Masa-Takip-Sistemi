93afc00b20116422e43d351bc11c93740c46635e
Ertuğrul EVLİYAOĞLU <153455312+ErTev@users.noreply.github.com>
2026-06-05 14:43:48 +0300

    First Commit

--- LibrarySeatTrackingAPI_Backend/obj/Debug/net9.0/LibrarySeatTrackingAPI.AssemblyInfo.cs
+++ LibrarySeatTrackingAPI_Backend/obj/Debug/net9.0/LibrarySeatTrackingAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySeatTrackingAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1205ef73b9f1fb8595f38c5b456daaf809e48bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySeatTrackingAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySeatTrackingAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
